e6d3cb7ff164e5d22f1badcfc0818b2c115d0fb7
seryoga279 <seryoga279@gmail.com>
2014-05-14 20:43:09 +0900

    new commit

--- src/DOMIC/GeneratePerl.java
+++ src/DOMIC/GeneratePerl.java
@@ -19,32 +19,31 @@ public class GeneratePerl {
     StringBuilder parseToPerl = new StringBuilder();
     
     GeneratePerl(ArrayList<QuestionBlock> questionBlock){
-       
-             this.parseToPerl.append("$test = [");
-             for(int i=0; i<questionBlock.size(); i++){ 
-                 this.parseToPerl.append("[");
-             
-        for (int j=0; j<questionBlock.get(i).questions.size(); j++){
-            
-             
-             this.parseToPerl.append("{-qu=> '" + questionBlock.get(i).getQuestionName(j) + "'-sa [=>");
-            
-            for(int k=0; k<questionBlock.get(i).questions.get(j).answers.size(); k++){
-              
-                this.parseToPerl.append("'" + questionBlock.get(i).getAnswerText(j, k) + "',");
-            }
-            this.parseToPerl.deleteCharAt(this.parseToPerl.length()-1);
-            
-             this.parseToPerl.append("]}");
-            
-        }
-       
-       
-             this.parseToPerl.append("]]");
         
-        
-    }
-    }
+       this.parseToPerl.append("$test = [");
+       for(int i=0; i<questionBlock.size(); i++){ 
+           this.parseToPerl.append("[");
+           
+           for (int j=0; j<questionBlock.get(i).questions.size(); j++){
+               
+                this.parseToPerl.append("{-qu=> '" + questionBlock.get(i).getQuestionName(j) + "'-sa [=>");
+                                
+                for(int k=0; k<questionBlock.get(i).questions.get(j).answers.size(); k++){
+                                  
+                    this.parseToPerl.append("'" + questionBlock.get(i).getAnswerText(j, k) + "',");
+                    
+                    }
+                this.parseToPerl.deleteCharAt(this.parseToPerl.length()-1);
+                this.parseToPerl.append("]}");
+                                
+                }
+           this.parseToPerl.append("]");
+           
+           
+           }
+       this.parseToPerl.append("]");
+       }
+    
      public StringBuilder getPerl(){
          
          return this.parseToPerl;
