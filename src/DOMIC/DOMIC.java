983ab66b768da257afd63ff2061ddf9dfe0182a5
seryoga279 <seryoga279@gmail.com>
2014-05-26 13:47:38 +0900

    new commit

--- src/DOMIC/DOMIC.java
+++ src/DOMIC/DOMIC.java
@@ -18,7 +18,7 @@ public class DOMIC {
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new StartPage().setVisible(true);
+                new DomicTestCreator().setVisible(true);
             }
         });
     }
--- src/DOMIC/EncodeJson.java
+++ src/DOMIC/EncodeJson.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DOMIC;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -17,103 +17,90 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 /**
  *
  * @author Сергей
  */
 public class EncodeJson {
-   private static final String jsonFilePath = "C:\\Users\\Сергей\\WebstormProjects\\untitled\\text.json";
 
- public static void main(String[] args) {
+    
+    public ArrayList<QuestionBlock> questionBlocks = new ArrayList<>();
 
-  JSONParser jsonParser = new JSONParser();
-
-  try {
-
-   FileReader fileReader = new FileReader(jsonFilePath);
-
-   JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
-
-   JSONArray test = (JSONArray) jsonObject.get("test");
-
-   
-for (int i=0; i<test.size(); i++){
-    JSONArray questionBlock;
-    questionBlock = (JSONArray) test.get(i);
-    for (int j=0; j<questionBlock.size(); j++){
-        HashMap questions = (HashMap) questionBlock.get(j);
-        Collection question;
-        question = questions.values();
-        Collection type;
-        type = questions.keySet();
-        Object values[] = question.toArray();
-        Object keys[] = type.toArray();
-       
-        JSONArray answerList = (JSONArray) values[1];
-         System.out.println(values[0]); 
-         
+    public EncodeJson(ArrayList<QuestionBlock> questionBlocks, String FilePatch) {
         
-       
-            if(String.valueOf(keys[1]).equals("-oa")){
-            
-                System.out.println(answerList.get(0));
-                
-            }
-            if(String.valueOf(keys[1]).equals("-sa")){
-                
-                
-                
-                for(int n=0; n<answerList.size(); n++){
-                    if(n==0)
-                        System.out.println(answerList.get(n) + " : true");
-                    else
-                        System.out.println(answerList.get(n) + " : false");
-                }
-                     
-                
-            }
-            if(String.valueOf(keys[1]).equals("-ma")){
-                
-                JSONArray trueAnswer = (JSONArray) answerList.get(0);
-                JSONArray falseAnswer = (JSONArray) answerList.get(1);
-                for (int n=0; n<trueAnswer.size();n++){
-                    
-                    System.out.println(trueAnswer.get(n) + " : true");
-                }
-                for (int n=0; n<falseAnswer.size();n++){
-                    
-                    System.out.println(falseAnswer.get(n) + " : false");
-                }
-                
-                
-            }
-       
         
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+
+            FileReader fileReader = new FileReader(FilePatch);
+
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
+
+            JSONArray test = (JSONArray) jsonObject.get("test");
+
+            for (int i = 0; i < test.size(); i++) {
+                JSONArray questionBlock;
+                this.questionBlocks.add(new QuestionBlock());
+                questionBlock = (JSONArray) test.get(i);
+
+                for (int j = 0; j < questionBlock.size(); j++) {
+                    HashMap questions = (HashMap) questionBlock.get(j);
+                    Collection question;
+                    question = questions.values();
+                    Collection type;
+                    type = questions.keySet();
+                    Object values[] = question.toArray();
+                    Object keys[] = type.toArray();
+
+                    JSONArray answerList = (JSONArray) values[1];
+
+                    this.questionBlocks.get(i).questions.add(new Question((String) values[0]));
+
+                    if (String.valueOf(keys[1]).equals("-oa")) {
+
+                        this.questionBlocks.get(i).questions.get(j).addAnswer(true, String.valueOf(answerList.get(0)));
+
+                    }
+                    if (String.valueOf(keys[1]).equals("-sa")) {
+
+                        for (int n = 0; n < answerList.size(); n++) {
+                            if (n == 0) {
+                                this.questionBlocks.get(i).questions.get(j).addAnswer(true, String.valueOf(answerList.get(n)));
+                            } else {
+                                this.questionBlocks.get(i).questions.get(j).addAnswer(true, String.valueOf(answerList.get(n)));
+                            }
+                        }
+
+                    }
+                    if (String.valueOf(keys[1]).equals("-ma")) {
+
+                        JSONArray trueAnswer = (JSONArray) answerList.get(0);
+                        JSONArray falseAnswer = (JSONArray) answerList.get(1);
+                        for (int n = 0; n < trueAnswer.size(); n++) {
+
+                            this.questionBlocks.get(i).questions.get(j).addAnswer(true, String.valueOf(trueAnswer.get(n)));
+                        }
+                        for (int n = 0; n < falseAnswer.size(); n++) {
+
+                            this.questionBlocks.get(i).questions.get(j).addAnswer(false, String.valueOf(falseAnswer.get(n)));
+                        }
+
+                    }
+
+                }
+            }
+
+            System.out.println("");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
-}    
-      
-      
-  
-  
- 
-  
-    System.out.println("");
-  
-  
-   
-
-
-  } catch (FileNotFoundException e) {
-   e.printStackTrace();
-  } catch (IOException e) {
-   e.printStackTrace();
-  } catch (ParseException e) {
-   e.printStackTrace();
-  }
-
- }
 
 }
-    
-
